3c09920e5714061ca5527202f03810128447c452
Ewayrton Oliveira <164266609+Ewayrton@users.noreply.github.com>
2024-08-30 23:44:35 +0000

    finalizado

--- ProgramacaoJS/index.js
+++ ProgramacaoJS/index.js
@@ -1,7 +1,6 @@
-const paragrafo = document.getElementById(".parag1");
-
-
 function alterarCor (){
+    var paragrafo = document.getElementById("parag1");
     paragrafo.style.color = "blue";
+    paragrafo.innerText = "Meu parágrafo agora é azul !"
 
 }
